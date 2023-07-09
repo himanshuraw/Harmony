1da07cfab8e36dc447190500ba1dfc50a20880c1
Himanshu Rawat <himanshurawat180@gmail.com>
2023-07-09 18:22:10 +0530

    Back to default

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { createBrowserRouter } from 'react-router-dom';
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-	},
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
