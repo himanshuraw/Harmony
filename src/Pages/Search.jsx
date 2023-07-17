95655292e8d2fd0834ea174b886d9c87c6e4fa8c
Himanshu Rawat <himanshurawat180@gmail.com>
2023-07-17 21:27:42 +0530

    Search > browse all

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -11,17 +11,20 @@ const Search = () => {
 	const { token } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
 	useEffect(() => {
+		const params = {
+			limit: 50,
+		};
 		setLoading(true);
 		if (token === '') {
 			getAuth().then((result) => {
 				dispatch(getToken(result));
-				fetchData('/browse/categories', result).then((data) => {
+				fetchData('/browse/categories', result, params).then((data) => {
 					dispatch(getCategories(data));
 					setLoading(false);
 				});
 			});
 		} else {
-			fetchData('/browse/categories', token).then((data) => {
+			fetchData('/browse/categories', token, params).then((data) => {
 				dispatch(getCategories(data));
 				setLoading(false);
 			});
@@ -29,20 +32,26 @@ const Search = () => {
 	}, []);
 
 	return (
-		<div className='h-[200vh]'>
+		<section className='flex flex-col'>
+			<div className='font-black text-xl mt-8 mb-4'>Browse all</div>
 			{loading === true ? (
 				<div>loading...</div>
 			) : (
-				<div>
+				<div className='grid grid-cols-4  lg:grid-cols-7 gap-6'>
 					{categories?.map((category) => (
-						<div key={category?.id}>
+						<div
+							key={category?.id}
+							className='rounded-lg bg-slate-300 overflow-hidden relative'
+						>
 							<img src={category?.icons[0]?.url} />
-							<div>{category?.name}</div>
+							<span className='absolute p-4 top-0 left-0 text-xl font-black'>
+								{category?.name}
+							</span>
 						</div>
 					))}
 				</div>
 			)}
-		</div>
+		</section>
 	);
 };
 
