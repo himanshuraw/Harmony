bc0bd1bf613b9f69b8c398a1be2dd6e6b1287bad
Himanshu Rawat <himanshurawat180@gmail.com>
2023-07-28 11:24:40 +0530

    applied onClick on banner items

--- src/components/BannerItems.jsx
+++ src/components/BannerItems.jsx
@@ -1,13 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import Image from './Image';
 
 const BannerItems = ({ data }) => {
+	const navigate = useNavigate();
 	return (
 		<>
 			<div className='grid grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-3 lg:gap-y-4'>
 				{data?.map((d) => (
 					<div
 						key={d?.id}
-						className='flex bg-white bg-opacity-10 rounded-[4px] overflow-hidden'
+						className='flex bg-white bg-opacity-10 rounded-[4px] overflow-hidden cursor-pointer'
+						onClick={() => navigate(`/playlist/${d.id}`)}
 					>
 						<div className='h-16 lg:h-20 aspect-square'>
 							<Image src={d?.images[0]?.url} />
